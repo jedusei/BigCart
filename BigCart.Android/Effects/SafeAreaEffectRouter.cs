60f978fad61cf063898411d54fd9cc6740ad6c41
Joseph Edusei <jedusei99@gmail.com>
2021-08-29 11:05:39 +0000

    Added and implemented SafeAreaEffect on Android.

---
+++ BigCart.Android/Effects/SafeAreaEffectRouter.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Views;
+using BigCart.Droid.Effects;
+using BigCart.Effects;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using View = Xamarin.Forms.View;
+
+[assembly: ResolutionGroupName(SafeAreaEffect.GroupName)]
+[assembly: ExportEffect(typeof(SafeAreaEffectRouter), SafeAreaEffect.Name)]
+namespace BigCart.Droid.Effects
+{
+    public class SafeAreaEffectRouter : PlatformEffect
+    {
+        public new View Element => (View)base.Element;
+
+        protected override void OnAttached()
+        {
+            WindowInsets insets = Container.Context.GetActivity().Window.DecorView.RootWindowInsets;
+            SafeArea safeArea = SafeAreaEffect.GetSafeArea(Element);
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            Element.Margin = new Thickness(
+                CalculateInsets(insets.StableInsetLeft, safeArea.Left),
+                CalculateInsets(insets.StableInsetTop, safeArea.Top),
+                CalculateInsets(insets.StableInsetRight, safeArea.Right),
+                CalculateInsets(insets.StableInsetBottom, safeArea.Bottom)
+            );
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+
+        protected override void OnDetached()
+        {
+            Element.Margin = 0;
+        }
+
+        private double CalculateInsets(double insetsComponent, bool shouldUseInsetsComponent)
+        {
+            return shouldUseInsetsComponent ? insetsComponent : 0;
+        }
+    }
+}
---
+++ BigCart/Effects/SafeAreaEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Forms;
+
+namespace BigCart.Effects
+{
+    public class SafeAreaEffect : RoutingEffect
+    {
+        public const string GroupName = "BigCart";
+        public const string Name = nameof(SafeAreaEffect); 
+        public static readonly BindableProperty SafeAreaProperty = BindableProperty.CreateAttached("SafeArea", typeof(SafeArea), typeof(SafeAreaEffect), default(SafeArea), propertyChanged: OnSafeAreaChanged);
+
+        public SafeAreaEffect() : base($"{GroupName}.{Name}")
+        {
+        }
+
+        public static SafeArea GetSafeArea(BindableObject view)
+        => (SafeArea)view.GetValue(SafeAreaProperty);
+
+        public static void SetSafeArea(BindableObject view, SafeArea value)
+            => view.SetValue(SafeAreaProperty, value);
+
+        private static void OnSafeAreaChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is View view)
+            {
+                Effect oldEffect = view.Effects.FirstOrDefault(e => e is SafeAreaEffect);
+
+                if (oldEffect != null)
+                    view.Effects.Remove(oldEffect);
+
+                if (((SafeArea)newValue).IsEmpty)
+                    return;
+
+                view.Effects.Add(new SafeAreaEffect());
+            }
+        }
+    }
+}
