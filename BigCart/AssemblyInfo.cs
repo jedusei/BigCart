abfbe8888e6c49bef26aabaaf80d63daba1823c5
Joseph Edusei <jedusei99@gmail.com>
2021-08-29 08:56:05 +0000

    Added fonts, colors & some styles.

--- BigCart/AssemblyInfo.cs
+++ BigCart/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Poppins-Regular.ttf", Alias = "Poppins")]
+[assembly: ExportFont("Poppins-Bold.ttf", Alias = "Poppins-Bold")]
