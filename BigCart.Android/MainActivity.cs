72405643130ad59b4abb2585bab8c98bd13fc1b8
Joseph Edusei <jedusei99@gmail.com>
2021-08-29 09:37:50 +0000

    Added splash screen on Android.

--- BigCart.Android/MainActivity.cs
+++ BigCart.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Runtime;
 
 namespace BigCart.Droid
 {
-    [Activity(Label = "BigCart", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ BigCart.Android/SplashActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using AndroidX.AppCompat.App;
+using System.Threading.Tasks;
+
+namespace BigCart.Droid
+{
+    [Activity(MainLauncher = true, NoHistory = true, Theme = "@style/SplashTheme")]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override async void OnResume()
+        {
+            base.OnResume();
+
+            await Task.Delay(2000);
+
+            StartActivity(typeof(MainActivity));
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
