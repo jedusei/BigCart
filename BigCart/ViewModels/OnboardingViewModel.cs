8dd3ec1a037ed4d38d757b27db326a7cac004c17
Joseph Edusei <jedusei99@gmail.com>
2021-08-29 09:39:17 +0000

    Added initial onboarding screen design.

--- BigCart/ViewModels/OnboardingViewModel.cs
+++ BigCart/ViewModels/OnboardingViewModel.cs
@@ -1,7 +1,17 @@
-﻿namespace BigCart.ViewModels
+﻿using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace BigCart.ViewModels
 {
     public class OnboardingViewModel : ViewModel
     {
+        public ICommand GetStartedCommand { get; }
+
+        public OnboardingViewModel()
+        {
+          //  GetStartedCommand = new AsyncCommand(()=>_navigationService.InitializeAsync)
+        }
+
         public override void Initialize(object navigationData)
         {
             base.Initialize(navigationData);
--- BigCart/Views/Pages/OnboardingPage.xaml.cs
+++ BigCart/Views/Pages/OnboardingPage.xaml.cs
@@ -1,16 +1,43 @@
-﻿using BigCart.Views;
+﻿using Xamarin.Forms;
 
-namespace BigCart.Pages
+namespace BigCart.Views
 {
     public partial class OnboardingPage : Page
     {
         public OnboardingPage()
         {
             InitializeComponent();
+            SetPages();
         }
 
-        private void Button_Clicked(object sender, System.EventArgs e)
+        private void SetPages()
         {
+            carouselView.ItemsSource = new[] {
+                new
+                {
+                    ShowLogo = true,
+                    Title = "Welcome to",
+                    Image = ImageSource.FromFile("onboarding_1.png")
+                },
+                new
+                {
+                    ShowLogo = false,
+                    Title = "Buy Quality Dairy Products",
+                    Image = ImageSource.FromFile("onboarding_2.png")
+                },
+                new
+                {
+                    ShowLogo = false,
+                    Title = "Buy Premium Quality Fruits",
+                    Image = ImageSource.FromFile("onboarding_3.png")
+                },
+                new
+                {
+                    ShowLogo = false,
+                    Title = "Get Discounts On All Products",
+                    Image = ImageSource.FromFile("onboarding_4.png")
+                }
+            };
         }
     }
 }
