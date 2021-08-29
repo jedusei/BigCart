0bf50ea5e21c86793b1041bb3456d8c70489f90e
Joseph Edusei <jedusei99@gmail.com>
2021-08-29 07:13:26 +0000

    Resolve navigation service in base view model constructor.

--- BigCart/ViewModels/Base/ViewModel.cs
+++ BigCart/ViewModels/Base/ViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using BigCart.DependencyInjection;
+using BigCart.Services.Navigation;
 using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace BigCart.ViewModels
 {
     public abstract class ViewModel : ObservableObject, IDependency
     {
+        protected readonly INavigationService _navigationService;
+
+        public ViewModel()
+        {
+            _navigationService = DependencyResolver.Get<INavigationService>();
+        }
+
         public virtual void Initialize(object navigationData) { }
         public virtual void OnStart() { }
         public virtual void OnResume() { }
