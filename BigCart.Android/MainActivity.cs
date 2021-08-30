5bea9a505dd8a26fd4c8ba8cc3e4d39b4901813f
Joseph Edusei <jedusei99@gmail.com>
2021-08-30 22:14:55 +0000

    Remove splash screen activity. set splash screen as main avtivity background, and fix system bar color issues.

--- BigCart.Android/MainActivity.cs
+++ BigCart.Android/MainActivity.cs
@@ -7,19 +7,28 @@ using Android.Views;
 namespace BigCart.Droid
 {
     [Activity(
+        MainLauncher = true,
         Theme = "@style/MainTheme",
         ScreenOrientation = ScreenOrientation.Portrait,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize
     )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private static bool _isFirstTime = true;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+            SetupWindow();
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            if (!_isFirstTime)
+                System.Threading.Thread.Sleep(1500);
+            else
+                _isFirstTime = false;
+
             LoadApplication(new App());
         }
 
@@ -35,6 +44,25 @@ namespace BigCart.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void SetupWindow()
+        {
+            SystemUiFlags systemUiVisibility = SystemUiFlags.LightStatusBar
+                | SystemUiFlags.LayoutStable
+                | SystemUiFlags.LayoutFullscreen
+                | SystemUiFlags.LayoutHideNavigation;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+                systemUiVisibility |= SystemUiFlags.LightNavigationBar;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)systemUiVisibility;
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            var winParams = Window.Attributes;
+            winParams.Flags &= ~(WindowManagerFlags.TranslucentStatus | WindowManagerFlags.TranslucentNavigation);
+            Window.Attributes = winParams;
         }
     }
 }
