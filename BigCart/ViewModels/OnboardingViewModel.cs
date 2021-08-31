c7e392c48e9526b60fd09f6fe4b4ea5075f2f826
Joseph Edusei <jedusei99@gmail.com>
2021-08-31 03:34:16 +0000

    Implemented next button functionality on onboarding screen.

--- BigCart/ViewModels/OnboardingViewModel.cs
+++ BigCart/ViewModels/OnboardingViewModel.cs
@@ -1,27 +1,58 @@
-﻿using BigCart.Services.Navigation;
-using BigCart.Pages;
+﻿using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
 
 namespace BigCart.ViewModels
 {
     public class OnboardingViewModel : ViewModel
     {
-        static int count;
-        public ICommand GetStartedCommand { get; }
+        private int _currentSlideIndex;
+
+        public object[] Slides { get; } = new[] {
+            new
+            {
+                ShowLogo = true,
+                Title = "Welcome to",
+                Image = ImageSource.FromFile("onboarding_1.png")
+            },
+            new
+            {
+                ShowLogo = false,
+                Title = "Buy Quality Dairy Products",
+                Image = ImageSource.FromFile("onboarding_2.png")
+            },
+            new
+            {
+                ShowLogo = false,
+                Title = "Buy Premium Quality Fruits",
+                Image = ImageSource.FromFile("onboarding_3.png")
+            },
+            new
+            {
+                ShowLogo = false,
+                Title = "Get Discounts On All Products",
+                Image = ImageSource.FromFile("onboarding_4.png")
+            }
+        };
+        public int CurrentSlideIndex
+        {
+            get => _currentSlideIndex;
+            set => SetProperty(ref _currentSlideIndex, value);
+        }
+        public ICommand NextCommand { get; }
 
         public OnboardingViewModel()
         {
-            GetStartedCommand = new AsyncCommand(() => _navigationService.PushAsync<OnboardingPage>(new NavigationOptions { ClearHistory = (count > 3) }));
+            NextCommand = new AsyncCommand(NextSlideAsync);
         }
 
-        public override void Initialize(object navigationData)
+        private async Task NextSlideAsync()
         {
-            base.Initialize(navigationData);
-            if (count > 3)
-                count = 0;
+            if (_currentSlideIndex < Slides.Length - 1)
+                CurrentSlideIndex++;
 
-            count++;
+            await Task.CompletedTask;
         }
     }
 }
--- BigCart/Views/Pages/OnboardingPage.xaml.cs
+++ BigCart/Views/Pages/OnboardingPage.xaml.cs
@@ -1,43 +1,10 @@
-﻿using Xamarin.Forms;
-
-namespace BigCart.Pages
+﻿namespace BigCart.Pages
 {
     public partial class OnboardingPage : Page
     {
         public OnboardingPage()
         {
             InitializeComponent();
-            SetPages();
-        }
-
-        private void SetPages()
-        {
-            carouselView.ItemsSource = new[] {
-                new
-                {
-                    ShowLogo = true,
-                    Title = "Welcome to",
-                    Image = ImageSource.FromFile("onboarding_1.png")
-                },
-                new
-                {
-                    ShowLogo = false,
-                    Title = "Buy Quality Dairy Products",
-                    Image = ImageSource.FromFile("onboarding_2.png")
-                },
-                new
-                {
-                    ShowLogo = false,
-                    Title = "Buy Premium Quality Fruits",
-                    Image = ImageSource.FromFile("onboarding_3.png")
-                },
-                new
-                {
-                    ShowLogo = false,
-                    Title = "Get Discounts On All Products",
-                    Image = ImageSource.FromFile("onboarding_4.png")
-                }
-            };
         }
     }
 }
