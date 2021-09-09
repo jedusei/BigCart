807b79a5085f6a762bbe005d9c96744550280193
Joseph Edusei <jedusei99@gmail.com>
2021-09-09 01:36:48 +0000

    Implemented sign out function on account tab.

---
+++ BigCart/ViewModels/AccountViewModel.cs
@@ -0,0 +1,34 @@
+﻿using BigCart.Pages;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace BigCart.ViewModels
+{
+    public class AccountViewModel : ViewModel
+    {
+        public ICommand OpenMenuItemCommand { get; }
+
+        public AccountViewModel()
+        {
+            OpenMenuItemCommand = new AsyncCommand<string>(OpenMenuItemAsync, allowsMultipleExecutions: false);
+        }
+
+        private async Task OpenMenuItemAsync(string menuItemId)
+        {
+            switch (menuItemId)
+            {
+                case AccountTab.MENU_ITEM_LOGOUT:
+                    await LogoutAsync();
+                    break;
+            }
+        }
+
+        private async Task LogoutAsync()
+        {
+            bool confirmed = await _modalService.ConfirmAsync("Are you sure you want to sign out?", "Sign Out");
+            if (confirmed)
+                await _navigationService.PushAsync<WelcomePage>(new() { ClearHistory = true });
+        }
+    }
+}
