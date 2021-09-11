ef487ee5310e7def0f3ded11f65764e8f32d82e6
Joseph Edusei <jedusei99@gmail.com>
2021-09-11 17:24:41 +0000

    Apply global rating style.

---
+++ BigCart/Controls/RatingEx.cs
@@ -0,0 +1,15 @@
+﻿using Syncfusion.SfRating.XForms;
+using Xamarin.Forms;
+
+namespace BigCart.Controls
+{
+    public class RatingEx : SfRating
+    {
+        public RatingEx()
+        {
+            var style = App.Current.Resources["Rating"] as Style;
+            foreach (Setter setter in style.Setters)
+                SetValue(setter.Property, setter.Value);
+        }
+    }
+}
