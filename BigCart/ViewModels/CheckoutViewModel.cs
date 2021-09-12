a48c04de80735c685825f077b6a7e57fd134f811
Joseph Edusei <jedusei99@gmail.com>
2021-09-12 20:34:12 +0000

    Added next button functionality to checkout page.

--- BigCart/ViewModels/CheckoutViewModel.cs
+++ BigCart/ViewModels/CheckoutViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace BigCart.ViewModels
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace BigCart.ViewModels
 {
     public class CheckoutViewModel : ViewModel
     {
         private int _currentStep;
+        private bool _isComplete;
 
         public string Title { get; private set; }
         public int CurrentStep
@@ -10,10 +15,17 @@
             get => _currentStep;
             set => SetProperty(ref _currentStep, value, onChanged: UpdateTitle);
         }
+        public bool IsComplete
+        {
+            get => _isComplete;
+            set => SetProperty(ref _isComplete, value);
+        }
+        public ICommand NextStepCommand { get; }
 
         public CheckoutViewModel()
         {
             UpdateTitle();
+            NextStepCommand = new AsyncCommand(NextStepAsync, allowsMultipleExecutions: false);
         }
 
         private void UpdateTitle()
@@ -26,6 +38,19 @@
             };
 
             OnPropertyChanged(nameof(Title));
+        }
+
+        private async Task NextStepAsync()
+        {
+            if (_currentStep < 2)
+                CurrentStep++;
+            else
+            {
+                IsComplete = true;
+                _modalService.ShowLoading("Making payment...");
+                await Task.Delay(1000);
+                _modalService.HideLoading();
+            }
         }
     }
 }
--- BigCart/Views/Pages/CheckoutPage.xaml.cs
+++ BigCart/Views/Pages/CheckoutPage.xaml.cs
@@ -1,10 +1,27 @@
-﻿namespace BigCart.Pages
+﻿using BigCart.ViewModels;
+using Syncfusion.XForms.ProgressBar;
+
+namespace BigCart.Pages
 {
     public partial class CheckoutPage : Page
     {
+        CheckoutViewModel _viewModel;
+
         public CheckoutPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            _viewModel = BindingContext as CheckoutViewModel;
+        }
+
+        private void SfStepProgressBar_StepTapped(object sender, StepTappedEventArgs e)
+        {
+            if (e.Item.Status != StepStatus.NotStarted)
+                _viewModel.CurrentStep = e.Index;
         }
     }
 }
