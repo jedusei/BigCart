241a581144a4d1457a82fbbc634656c16d47ae98
Joseph Edusei <jedusei99@gmail.com>
2021-09-12 14:51:43 +0000

    Added cart service.

---
+++ BigCart/Services/Cart/CartService.cs
@@ -0,0 +1,47 @@
+﻿using BigCart.DependencyInjection;
+using BigCart.Models;
+using BigCart.Services.Products;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigCart.Services.Cart
+{
+    public class CartService : ICartService, ISingletonDependency
+    {
+        private readonly IProductService _productService;
+        private ObservableCollection<Product> _items;
+        private ReadOnlyObservableCollection<Product> _itemsReadonly;
+
+        public CartService(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<ReadOnlyObservableCollection<Product>> GetItemsAsync()
+        {
+            var delay = Task.Delay(1000);
+
+            if (_items == null)
+            {
+                Product[] products = await _productService.GetProductsAsync();
+                _items = new ObservableCollection<Product>(products.Where(p => p.IsInCart));
+                _itemsReadonly = new ReadOnlyObservableCollection<Product>(_items);
+            }
+
+            await delay;
+            return _itemsReadonly;
+        }
+
+        public void SetCartStatus(Product product, bool isInCart)
+        {
+            if (!isInCart)
+                _items.Remove(product);
+            else if (!_items.Contains(product))
+                _items.Add(product);
+
+            product.IsInCart = isInCart;
+            product.Quantity = isInCart ? 1 : 0;
+        }
+    }
+}
---
+++ BigCart/Services/Cart/ICartService.cs
@@ -0,0 +1,13 @@
+﻿using BigCart.DependencyInjection;
+using BigCart.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace BigCart.Services.Cart
+{
+    public interface ICartService : IDependency
+    {
+        Task<ReadOnlyObservableCollection<Product>> GetItemsAsync();
+        void SetCartStatus(Product product, bool isInCart);
+    }
+}
