a76dae4c1904c4ef1b0907d2eccb29814f969bcf
Joseph Edusei <jedusei99@gmail.com>
2021-09-12 15:22:05 +0000

    Added SwipeViewEx control, which only allows one swipeview to be active at a time.

---
+++ BigCart/Controls/SwipeViewEx.cs
@@ -0,0 +1,51 @@
+﻿using Xamarin.Forms;
+
+namespace BigCart.Controls
+{
+    public class SwipeViewEx : SwipeView
+    {
+        public const string MSG_OPENED = nameof(MSG_OPENED);
+        public const string MSG_CLOSE = nameof(MSG_CLOSE);
+
+        private Page Page
+        {
+            get
+            {
+                Element parent = Parent;
+
+                while (parent != null)
+                {
+                    if (parent is Page page)
+                        return page;
+
+                    parent = parent.Parent;
+                }
+
+                return null;
+            }
+        }
+
+        public SwipeViewEx()
+        {
+            SwipeEnded += OnSwipeEnded;
+
+            MessagingCenter.Subscribe<SwipeViewEx, Page>(this, MSG_OPENED, (sender, page) =>
+            {
+                if (sender != this && page == Page)
+                    Close();
+            });
+
+            MessagingCenter.Subscribe<object, object>(this, MSG_CLOSE, (_, bc) =>
+            {
+                if (bc == BindingContext)
+                    Close();
+            });
+        }
+
+        private void OnSwipeEnded(object sender, SwipeEndedEventArgs e)
+        {
+            if (e.IsOpen)
+                MessagingCenter.Send(this, MSG_OPENED, Page);
+        }
+    }
+}
