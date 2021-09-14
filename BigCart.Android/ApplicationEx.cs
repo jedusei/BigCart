0e68f1a1359f78c9996739b337fe5cbdcc45b309
Joseph Edusei <jedusei99@gmail.com>
2021-09-14 10:04:05 +0000

    Clear Android app cache when debugging starts, so that the font cache will be refreshed.

--- BigCart.Android/ApplicationEx.cs
+++ BigCart.Android/ApplicationEx.cs
@@ -5,12 +5,28 @@ using System;
 namespace BigCart.Droid
 {
     [Application]
-    class ApplicationEx : Application
+    public class ApplicationEx : Application
     {
         public ApplicationEx(IntPtr handle, JniHandleOwnership transer)
            : base(handle, transer)
         {
+            return;
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
             AndroidModule.Instance.Initialize();
+
+#if DEBUG
+            // Clear cache directory so that fonts will be updated.
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                string cacheDirectory = CacheDir.AbsolutePath;
+                System.IO.Directory.Delete(cacheDirectory, true);
+                System.IO.Directory.CreateDirectory(cacheDirectory);
+            }
+#endif
         }
     }
 }
