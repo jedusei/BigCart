318e6bec341f01e3b93061cdabb5be4fda28fa41
Joseph Edusei <jedusei99@gmail.com>
2021-09-20 16:55:39 +0000

    Added review model.

---
+++ BigCart/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigCart.Models
+{
+    public record Review
+    {
+        public User User { get; init; }
+        public float Rating { get; init; }
+        public string Comment { get; init; }
+        public DateTime DateCreated { get; init; } = DateTime.Now;
+    }
+}
