ae1a81c009270f78d482f6ebb4ebaeb3aee78d5a
Joseph Edusei <jedusei99@gmail.com>
2021-09-20 16:55:30 +0000

    Added user model.

---
+++ BigCart/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace BigCart.Models
+{
+    public class User : ObservableObject
+    {
+        private string _name;
+        private ImageSource _profilePicture;
+
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+        public ImageSource ProfilePicture
+        {
+            get => _profilePicture;
+            set => SetProperty(ref _profilePicture, value);
+        }
+    }
+}
