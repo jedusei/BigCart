2331353859e92ff0960669dd7f9de58ba1b279cb
Joseph Edusei <jedusei99@gmail.com>
2021-09-21 09:59:32 +0000

    Added transaction model.

---
+++ BigCart/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BigCart.Models
+{
+    public enum CreditCardType
+    {
+        Mastercard,
+        Visa,
+        Paypal
+    }
+
+    public record Transaction
+    {
+        private static int _nextId = 123456;
+
+        public int Id { get; } = _nextId++;
+        public CreditCardType CreditCardType { get; init; }
+        public float Amount { get; init; }
+        public DateTime DateCreated { get; init; } = DateTime.Now;
+    }
+}
