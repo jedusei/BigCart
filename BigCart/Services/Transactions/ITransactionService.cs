9e4a1b5b8e3490959aeaafb32bea892f2904d1c0
Joseph Edusei <jedusei99@gmail.com>
2021-09-21 10:41:18 +0000

    Added transaction service.

---
+++ BigCart/Services/Transactions/ITransactionService.cs
@@ -0,0 +1,12 @@
+﻿using BigCart.DependencyInjection;
+using BigCart.Models;
+using System.Threading.Tasks;
+
+namespace BigCart.Services.Transactions
+{
+    public interface ITransactionService : IDependency
+    {
+        Task<Transaction[]> GetTransactionsAsync();
+        Task RegisterTransactionAsync(Transaction transaction);
+    }
+}
---
+++ BigCart/Services/Transactions/TransactionService.cs
@@ -0,0 +1,52 @@
+﻿using BigCart.DependencyInjection;
+using BigCart.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BigCart.Services.Transactions
+{
+    public class TransactionService : ITransactionService, ISingletonDependency
+    {
+        private readonly List<Transaction> _transactions = new()
+        {
+            new()
+            {
+                CreditCardType = CreditCardType.Mastercard,
+                Amount = 89.99f
+            },
+            new()
+            {
+                CreditCardType = CreditCardType.Visa,
+                Amount = 109.99f
+            },
+            new()
+            {
+                CreditCardType = CreditCardType.Paypal,
+                Amount = 567
+            },
+            new()
+            {
+                CreditCardType = CreditCardType.Paypal,
+                Amount = 480
+            },
+            new()
+            {
+                CreditCardType = CreditCardType.Mastercard,
+                Amount = 50
+            }
+        };
+
+        public async Task<Transaction[]> GetTransactionsAsync()
+        {
+            await Task.Delay(1000);
+            return _transactions.ToArray();
+        }
+
+        public async Task RegisterTransactionAsync(Transaction transaction)
+        {
+            await Task.Delay(1000);
+            if (_transactions.Find(t => t.Id == transaction.Id) == null)
+                _transactions.Insert(0, transaction);
+        }
+    }
+}
