202081809d2fc7be7ebc63038a8f2776ca65d87a
Joseph Edusei <jedusei99@gmail.com>
2021-09-21 10:45:21 +0000

    Added transactions page.

--- BigCart.Android/Resources/Resource.designer.cs
+++ BigCart.Android/Resources/Resource.designer.cs
@@ -18737,202 +18737,211 @@ namespace BigCart.Droid
 			public const int logo = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int material_ic_calendar_black_24dp = 2131165341;
+			public const int mastercard = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int material_ic_clear_black_24dp = 2131165342;
+			public const int material_ic_calendar_black_24dp = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int material_ic_edit_black_24dp = 2131165343;
+			public const int material_ic_clear_black_24dp = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int material_ic_keyboard_arrow_left_black_24dp = 2131165344;
+			public const int material_ic_edit_black_24dp = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int material_ic_keyboard_arrow_right_black_24dp = 2131165345;
+			public const int material_ic_keyboard_arrow_left_black_24dp = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int material_ic_menu_arrow_down_black_24dp = 2131165346;
+			public const int material_ic_keyboard_arrow_right_black_24dp = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int material_ic_menu_arrow_up_black_24dp = 2131165347;
+			public const int material_ic_menu_arrow_down_black_24dp = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int minus = 2131165348;
+			public const int material_ic_menu_arrow_up_black_24dp = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int mtrl_dialog_background = 2131165349;
+			public const int minus = 2131165349;
 			
 			// aapt resource value: 0x7F0700A6
-			public const int mtrl_dropdown_arrow = 2131165350;
+			public const int mtrl_dialog_background = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int mtrl_ic_arrow_drop_down = 2131165351;
+			public const int mtrl_dropdown_arrow = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int mtrl_ic_arrow_drop_up = 2131165352;
+			public const int mtrl_ic_arrow_drop_down = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int mtrl_ic_cancel = 2131165353;
+			public const int mtrl_ic_arrow_drop_up = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int mtrl_ic_error = 2131165354;
+			public const int mtrl_ic_cancel = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int mtrl_popupmenu_background = 2131165355;
+			public const int mtrl_ic_error = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int mtrl_popupmenu_background_dark = 2131165356;
+			public const int mtrl_popupmenu_background = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int mtrl_tabs_default_indicator = 2131165357;
+			public const int mtrl_popupmenu_background_dark = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int navigation_empty_icon = 2131165358;
+			public const int mtrl_tabs_default_indicator = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int notification_action_background = 2131165359;
+			public const int navigation_empty_icon = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int notification_bg = 2131165360;
+			public const int notification_action_background = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int notification_bg_low = 2131165361;
+			public const int notification_bg = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int notification_bg_low_normal = 2131165362;
+			public const int notification_bg_low = 2131165362;
 			
 			// aapt resource value: 0x7F0700B3
-			public const int notification_bg_low_pressed = 2131165363;
+			public const int notification_bg_low_normal = 2131165363;
 			
 			// aapt resource value: 0x7F0700B4
-			public const int notification_bg_normal = 2131165364;
+			public const int notification_bg_low_pressed = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int notification_bg_normal_pressed = 2131165365;
+			public const int notification_bg_normal = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int notification_icon_background = 2131165366;
+			public const int notification_bg_normal_pressed = 2131165366;
 			
 			// aapt resource value: 0x7F0700B7
-			public const int notification_template_icon_bg = 2131165367;
+			public const int notification_icon_background = 2131165367;
 			
 			// aapt resource value: 0x7F0700B8
-			public const int notification_template_icon_low_bg = 2131165368;
+			public const int notification_template_icon_bg = 2131165368;
 			
 			// aapt resource value: 0x7F0700B9
-			public const int notification_tile_bg = 2131165369;
+			public const int notification_template_icon_low_bg = 2131165369;
 			
 			// aapt resource value: 0x7F0700BA
-			public const int notify_panel_notification_icon_bg = 2131165370;
+			public const int notification_tile_bg = 2131165370;
 			
 			// aapt resource value: 0x7F0700BB
-			public const int onboarding_1 = 2131165371;
+			public const int notify_panel_notification_icon_bg = 2131165371;
 			
 			// aapt resource value: 0x7F0700BC
-			public const int onboarding_2 = 2131165372;
+			public const int onboarding_1 = 2131165372;
 			
 			// aapt resource value: 0x7F0700BD
-			public const int onboarding_3 = 2131165373;
+			public const int onboarding_2 = 2131165373;
 			
 			// aapt resource value: 0x7F0700BE
-			public const int onboarding_4 = 2131165374;
+			public const int onboarding_3 = 2131165374;
 			
 			// aapt resource value: 0x7F0700BF
-			public const int peach = 2131165375;
+			public const int onboarding_4 = 2131165375;
 			
 			// aapt resource value: 0x7F0700C0
-			public const int pineapple = 2131165376;
+			public const int paypal = 2131165376;
 			
 			// aapt resource value: 0x7F0700C1
-			public const int plus = 2131165377;
+			public const int peach = 2131165377;
 			
 			// aapt resource value: 0x7F0700C2
-			public const int pomegranate = 2131165378;
+			public const int pineapple = 2131165378;
 			
 			// aapt resource value: 0x7F0700C3
-			public const int preference_list_divider_material = 2131165379;
+			public const int plus = 2131165379;
 			
 			// aapt resource value: 0x7F0700C4
-			public const int roundedbg = 2131165380;
+			public const int pomegranate = 2131165380;
 			
 			// aapt resource value: 0x7F0700C5
-			public const int roundedbgdark = 2131165381;
+			public const int preference_list_divider_material = 2131165381;
 			
 			// aapt resource value: 0x7F0700C6
-			public const int sfcheckbox_animation = 2131165382;
+			public const int roundedbg = 2131165382;
 			
 			// aapt resource value: 0x7F0700C7
-			public const int sfcheckbox_checked = 2131165383;
+			public const int roundedbgdark = 2131165383;
 			
 			// aapt resource value: 0x7F0700C8
-			public const int sfcheckbox_checked_indeterminate = 2131165384;
+			public const int sfcheckbox_animation = 2131165384;
 			
 			// aapt resource value: 0x7F0700C9
-			public const int sfcheckbox_checked_unchecked = 2131165385;
+			public const int sfcheckbox_checked = 2131165385;
 			
 			// aapt resource value: 0x7F0700CA
-			public const int sfcheckbox_indeterminate = 2131165386;
+			public const int sfcheckbox_checked_indeterminate = 2131165386;
 			
 			// aapt resource value: 0x7F0700CB
-			public const int sfcheckbox_indeterminate_checked = 2131165387;
+			public const int sfcheckbox_checked_unchecked = 2131165387;
 			
 			// aapt resource value: 0x7F0700CC
-			public const int sfcheckbox_indeterminate_unchecked = 2131165388;
+			public const int sfcheckbox_indeterminate = 2131165388;
 			
 			// aapt resource value: 0x7F0700CD
-			public const int sfcheckbox_unchecked = 2131165389;
+			public const int sfcheckbox_indeterminate_checked = 2131165389;
 			
 			// aapt resource value: 0x7F0700CE
-			public const int sfcheckbox_unchecked_checked = 2131165390;
+			public const int sfcheckbox_indeterminate_unchecked = 2131165390;
 			
 			// aapt resource value: 0x7F0700CF
-			public const int sfcheckbox_unchecked_indeterminate = 2131165391;
+			public const int sfcheckbox_unchecked = 2131165391;
 			
 			// aapt resource value: 0x7F0700D0
-			public const int sfprogressbar_determinate_linear = 2131165392;
+			public const int sfcheckbox_unchecked_checked = 2131165392;
 			
 			// aapt resource value: 0x7F0700D1
-			public const int sfprogressbar_indeterminate_circular = 2131165393;
+			public const int sfcheckbox_unchecked_indeterminate = 2131165393;
 			
 			// aapt resource value: 0x7F0700D2
-			public const int sfprogressbar_indeterminate_linear = 2131165394;
+			public const int sfprogressbar_determinate_linear = 2131165394;
 			
 			// aapt resource value: 0x7F0700D3
-			public const int splash_logo = 2131165395;
+			public const int sfprogressbar_indeterminate_circular = 2131165395;
 			
 			// aapt resource value: 0x7F0700D4
-			public const int splash_screen = 2131165396;
+			public const int sfprogressbar_indeterminate_linear = 2131165396;
 			
 			// aapt resource value: 0x7F0700D5
-			public const int test_custom_background = 2131165397;
+			public const int splash_logo = 2131165397;
 			
 			// aapt resource value: 0x7F0700D6
-			public const int tooltip_frame_dark = 2131165398;
+			public const int splash_screen = 2131165398;
 			
 			// aapt resource value: 0x7F0700D7
-			public const int tooltip_frame_light = 2131165399;
+			public const int test_custom_background = 2131165399;
 			
 			// aapt resource value: 0x7F0700D8
-			public const int user_carla_g = 2131165400;
+			public const int tooltip_frame_dark = 2131165400;
 			
 			// aapt resource value: 0x7F0700D9
-			public const int user_carla_s = 2131165401;
+			public const int tooltip_frame_light = 2131165401;
 			
 			// aapt resource value: 0x7F0700DA
-			public const int user_haylie = 2131165402;
+			public const int user_carla_g = 2131165402;
 			
 			// aapt resource value: 0x7F0700DB
-			public const int user_maren = 2131165403;
+			public const int user_carla_s = 2131165403;
 			
 			// aapt resource value: 0x7F0700DC
-			public const int welcome_1 = 2131165404;
+			public const int user_haylie = 2131165404;
 			
 			// aapt resource value: 0x7F0700DD
-			public const int welcome_2 = 2131165405;
+			public const int user_maren = 2131165405;
 			
 			// aapt resource value: 0x7F0700DE
-			public const int welcome_3 = 2131165406;
+			public const int visa = 2131165406;
+			
+			// aapt resource value: 0x7F0700DF
+			public const int welcome_1 = 2131165407;
+			
+			// aapt resource value: 0x7F0700E0
+			public const int welcome_2 = 2131165408;
+			
+			// aapt resource value: 0x7F0700E1
+			public const int welcome_3 = 2131165409;
 			
 			static Drawable()
 			{
---
+++ BigCart/ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using BigCart.Models;
+using BigCart.Services.Transactions;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+
+namespace BigCart.ViewModels
+{
+    public class TransactionsViewModel : ViewModel
+    {
+        private readonly ITransactionService _transactionService;
+        private LayoutState _loadState;
+
+        public LayoutState LoadState
+        {
+            get => _loadState;
+            private set => SetProperty(ref _loadState, value);
+        }
+        public Transaction[] Transactions { get; private set; }
+
+        public TransactionsViewModel(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+            _ = LoadTransactionsAsync();
+        }
+
+        private async Task LoadTransactionsAsync()
+        {
+            LoadState = LayoutState.Loading;
+
+            Transactions = await _transactionService.GetTransactionsAsync();
+            OnPropertyChanged(nameof(Transactions));
+
+            LoadState = (Transactions.Length > 0) ? LayoutState.Success : LayoutState.Empty;
+        }
+    }
+}
