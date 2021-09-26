1ce93874b1dac3e053e426c8e473c199006fb4fb
Joseph Edusei <jedusei99@gmail.com>
2021-09-26 17:07:38 +0000

    Added "My Cards" page.

---
+++ BigCart/ViewModels/MyCardsViewModel.cs
@@ -0,0 +1,52 @@
+﻿using BigCart.Models;
+using BigCart.Services.CreditCards;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace BigCart.ViewModels
+{
+    public class MyCardsViewModel : ViewModel
+    {
+        private readonly ICreditCardService _creditCardService;
+        private LayoutState _loadState;
+
+        public LayoutState LoadState
+        {
+            get => _loadState;
+            set => SetProperty(ref _loadState, value);
+        }
+        public ReadOnlyObservableCollection<CreditCard> CreditCards { get; private set; }
+        public IAsyncCommand AddCardCommand { get; }
+        public ICommand SetDefaultCardCommand { get; }
+
+        public MyCardsViewModel(ICreditCardService creditCardService)
+        {
+            _creditCardService = creditCardService;
+            SetDefaultCardCommand = new Command<CreditCard>(card =>
+            {
+                if (!card.IsDefault)
+                    _creditCardService.SetDefaultCard(card.Number);
+            });
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+            _ = GetCardsAsync();
+        }
+
+        private async Task GetCardsAsync()
+        {
+            LoadState = LayoutState.Loading;
+
+            CreditCards = await _creditCardService.GetCardsAsync();
+            OnPropertyChanged(nameof(CreditCards));
+
+            LoadState = (CreditCards.Count > 0) ? LayoutState.Success : LayoutState.Empty;
+        }
+    }
+}
---
+++ BigCart/Views/Pages/MyCardsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using BigCart.Controls;
+using BigCart.Models;
+using BigCart.ViewModels;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace BigCart.Pages
+{
+    public partial class MyCardsPage : Page
+    {
+        public MyCardsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Expander_Tapped(object sender, System.EventArgs e)
+        {
+            var expander = (Expander)sender;
+            var dropdownIcon = expander.FindByName<View>("dropdownIcon");
+            dropdownIcon.CancelAnimations();
+            dropdownIcon.RotateTo(expander.IsExpanded ? -180 : 0, expander.AnimationLength, expander.AnimationEasing);
+        }
+    }
+}
