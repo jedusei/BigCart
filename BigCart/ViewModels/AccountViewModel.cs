2962bd0cfa3ae180ce47b99ace99561658c0636f
Joseph Edusei <jedusei99@gmail.com>
2021-09-27 11:19:01 +0000

    Linked account tab to cards page.

--- BigCart/ViewModels/AccountViewModel.cs
+++ BigCart/ViewModels/AccountViewModel.cs
@@ -32,6 +32,10 @@ namespace BigCart.ViewModels
                     MessagingCenter.Send((object)this, MessageKeys.GoToTab, MainPage.TAB_FAVORITES);
                     break;
 
+                case AccountTab.MENU_ITEM_CARDS:
+                    await _navigationService.PushAsync<MyCardsPage>();
+                    break;
+
                 case AccountTab.MENU_ITEM_TRANSACTIONS:
                     await _navigationService.PushAsync<TransactionsPage>();
                     break;
