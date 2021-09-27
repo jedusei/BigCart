cd9416299390493be42ca051f03183f070e7f2ed
Joseph Edusei <jedusei99@gmail.com>
2021-09-27 05:50:10 +0000

    Added credit card number mask converter.

--- BigCart/Controls/CreditCardView.xaml.cs
+++ BigCart/Controls/CreditCardView.xaml.cs
@@ -7,11 +7,10 @@ namespace BigCart.Controls
     public partial class CreditCardView : ContentView
     {
         public static readonly BindableProperty CardTypeProperty = BindableProperty.Create(nameof(CardType), typeof(CreditCardType), typeof(CreditCardView), CreditCardType.Mastercard, BindingMode.OneWay);
-        public static readonly BindableProperty CardNumberProperty = BindableProperty.Create(nameof(CardNumber), typeof(string), typeof(CreditCardView), CARD_NUMBER_PREFIX + "XXXX", propertyChanged: OnCardNumberChanged);
+        public static readonly BindableProperty CardNumberProperty = BindableProperty.Create(nameof(CardNumber), typeof(string), typeof(CreditCardView));
         public static readonly BindableProperty CardHolderProperty = BindableProperty.Create(nameof(CardHolder), typeof(string), typeof(CreditCardView), propertyChanged: OnCardHolderChanged);
         public static readonly BindableProperty ExpiryDateProperty = BindableProperty.Create(nameof(ExpiryDate), typeof(DateTime?), typeof(CreditCardView), propertyChanged: OnExpiryDateChanged);
-        private const string CARD_NUMBER_PREFIX = "XXXX XXXX XXXX ";
-        private Label _txtCardNumber, _txtCardHolder, _txtExpiryDate;
+        private Label _txtCardHolder, _txtExpiryDate;
 
         public CreditCardType CardType
         {
@@ -43,20 +42,13 @@ namespace BigCart.Controls
         {
             base.OnApplyTemplate();
 
-            _txtCardNumber = (Label)GetTemplateChild("txtCardNumber");
             _txtCardHolder = (Label)GetTemplateChild("txtCardHolder");
             _txtExpiryDate = (Label)GetTemplateChild("txtExpiryDate");
 
             UpdateCardHolder();
-            UpdateCardNumber();
             UpdateExpiryDate();
         }
 
-        private static void OnCardNumberChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            var view = (CreditCardView)bindable;
-            view.UpdateCardNumber();
-        }
         private static void OnCardHolderChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var view = (CreditCardView)bindable;
@@ -67,22 +59,6 @@ namespace BigCart.Controls
         {
             var view = (CreditCardView)bindable;
             view.UpdateExpiryDate();
-        }
-
-        private void UpdateCardNumber()
-        {
-            var cardNumber = CardNumber;
-            if (string.IsNullOrWhiteSpace(cardNumber))
-                _txtCardNumber.Text = (string)CardNumberProperty.DefaultValue;
-            else
-            {
-                string lastFourDigits = (cardNumber.Length >= 4) ? cardNumber.Substring(cardNumber.Length - 4) : cardNumber;
-
-                if (lastFourDigits.Length < 4)
-                    lastFourDigits = new string('X', 4 - lastFourDigits.Length) + lastFourDigits;
-
-                _txtCardNumber.Text = CARD_NUMBER_PREFIX + lastFourDigits;
-            }
         }
 
         private void UpdateCardHolder()
---
+++ BigCart/Converters/CreditCardNumberMaskConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BigCart.Converters
+{
+    public class CreditCardNumberMaskConverter : IValueConverter
+    {
+        private const string CARD_NUMBER_PREFIX = "XXXX XXXX XXXX ";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string cardNumber = value?.ToString().Trim();
+            if (string.IsNullOrEmpty(cardNumber))
+                cardNumber = "XXXX";
+
+            string lastFourDigits = (cardNumber.Length >= 4) ? cardNumber.Substring(cardNumber.Length - 4) : cardNumber;
+
+            if (lastFourDigits.Length < 4)
+                lastFourDigits = new string('X', 4 - lastFourDigits.Length) + lastFourDigits;
+
+            return CARD_NUMBER_PREFIX + lastFourDigits;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
